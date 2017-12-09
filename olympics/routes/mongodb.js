7329bc51b0f9e68355b1304194fdf14b307bd6f5
BearBurg <ruqing@seas.upenn.edu>
2017-12-09 16:51:46 -0500

    delete unused files

--- olympics/routes/mongodb.js
+++
@@ -1,5 +0,0 @@
-var mongojs = require('mongojs');
-var connectionString = "mongodb://cis550:cis550@cis550-shard-00-00-a0sk1.mongodb.net:27017,cis550-shard-00-01-a0sk1.mongodb.net:27017,cis550-shard-00-02-a0sk1.mongodb.net:27017/data?ssl=true&replicaSet=Cis550-shard-0&authSource=admin"
-var db = mongojs(connectionString, ['olympics_new']);
-
-
